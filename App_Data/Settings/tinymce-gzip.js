4f3bb5db087fd415e12a3bdccdf2145132bad445
Mattias Uhlegard <mattias@uhlegard.se>
2013-04-12 13:10:17 +0200

    Removed App_Data filder and update to .gitignore

--- App_Data/Settings/tinymce-gzip.js
+++
@@ -1,9 +0,0 @@
-﻿<script type="text/javascript"> 
-tinyMCE_GZ.init({
-  plugins: "contextmenu,fullscreen,media,pagebreak,paste,table,codemirror,headings,lemoonimage,lemoonlink,pdw",
-  themes: "advanced",
-  languages: "en,sv",
-  disk_cache: true,
-  debug: false
-});
-</script>
