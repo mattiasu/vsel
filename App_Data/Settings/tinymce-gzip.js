470e5c8f93b27c8dcb30c0c62ddaac1d97fe3e42
Mattias Uhlegard <mattias@uhlegard.se>
2013-04-15 23:33:34 +0200

    changes in gitignore and project

---
+++ App_Data/Settings/tinymce-gzip.js
@@ -0,0 +1,9 @@
+﻿<script type="text/javascript"> 
+tinyMCE_GZ.init({
+  plugins: "contextmenu,fullscreen,media,pagebreak,paste,table,codemirror,headings,lemoonimage,lemoonlink,pdw",
+  themes: "advanced",
+  languages: "en,sv",
+  disk_cache: true,
+  debug: false
+});
+</script>
